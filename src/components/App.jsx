b50e3d56a4526b21183e4cbd6727520463ebbaf5
R0m5 <rlastovetskyi@gmail.com>
2023-06-09 17:43:37 +0300

    Done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,42 @@
+import { ContactForm } from './ContactForm/ContactForm';
+import { ContactList } from './ContactsList/ContactsList';
+import { Filter } from './Filter/Filter';
+import css from './App.module.css';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchContacts } from 'redux/operation';
+import { selectError, selectIsLoading } from 'redux/selectors';
+
 export const App = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
     <div
       style={{
-        height: '100vh',
         display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
+        flexDirection: 'column',
+        alignItems: 'start',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        width: '480px',
+        margin: '20px auto',
+        backgroundColor: ' rgb(187, 187, 187)',
+        borderRadius: '10px',
+        padding: '40px 20px',
       }}
     >
-      React homework template
+      <h1 className={css.phohebookTitle}>Phonebook</h1>
+      <ContactForm />
+      <h2 className={css.phohebookTitle}>Contacts</h2>
+      <Filter />
+      {isLoading && !error && <b className={css.loader}>Loading...</b>}
+      <ContactList />
     </div>
   );
 };
